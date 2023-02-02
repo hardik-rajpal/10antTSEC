eebce5ad5a4250fbd5346e1ea5371a89fd892950
Ashwin <200050016@iitb.ac.in>
2023-02-02 13:38:33 +0100

    some more changes?

--- app/lib/landlord/stats_flats.dart
+++ app/lib/landlord/stats_flats.dart
@@ -11,23 +11,125 @@ class StatsFlats extends StatefulWidget {
 }
 
 class _StatsFlatsState extends State<StatsFlats> {
-  
+  List fooList = ['one', 'two', 'three', 'four', 'five'];
+  List filteredList = [];
+  @override
+  void initState() {
+    super.initState();
+    filteredList = fooList;
+  }
+
+  void filter(String inputString) {
+    filteredList =
+        fooList.where((i) => i.toLowerCase().contains(inputString)).toList();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(title: Text("Add Flat")),
+            body: Padding(
+                padding: EdgeInsets.all(30.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                          height: 20,
+                          child: Column(children: [
+                            Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Icon(Icons.check),
+                                  Icon(Icons.close),
+                                  Icon(Icons.question_mark)
+                                ]),
+                            Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text("12"),
+                                  Text("2222"),
+                                  Text("e2"),
+                                ])
+                          ])),
+                      Container(
+                        height: 10,
+                        child: Column(children: [
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Search ',
+                              hintStyle: TextStyle(
+                                fontSize: 14,
+                              ),
+                            ),
+                            onChanged: (text) {
+                              text = text.toLowerCase();
+                              filter(text);
+                            },
+                          ),
+                          SingleChildScrollView(
+                            child: ListView.builder(
+                              padding: const EdgeInsets.all(5.5),
+                              itemCount: 5,
+                              itemBuilder: _itemBuilder,
+                              //(BuildContext context, int index) =>
+                              //     ListTile(
+                              //   title: Text(filteredList[index]),
+                              // onTap: () {
+                              //   Navigator.push(
+                              //     context,
+                              //     MaterialPageRoute(
+                              //       builder: (context) => Display(
+                              //         text: filteredList[index],
+                              //       ),
+                              //     ),
+                              //   );
+                              // },
+                              // ),
+                            ),
+                          ),
+                        ]),
+                      )
+                    ],
+                  ),
+                ))));
+  }
+}
+
+Widget _itemBuilder(BuildContext context, int index) {
+  return InkWell(
+    child: Card(
+      margin: const EdgeInsets.all(5),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            title: Text("index"),
+            onTap: () {},
+          ),
+        ],
+      ),
+    ),
+    // onTap: () => MaterialPageRoute(
+    //     builder: (context) =>
+    //         SecondRoute(id: _data.getId(index), name: _data.getName(index))),
+  );
+}
+
+class Display extends StatelessWidget {
+  final String text;
+
+  const Display({required this.text});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Add Flat")),
-        body: SingleChildScrollView(
-            child: Padding(
-                padding: EdgeInsets.all(70.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                  Icon(
-                    Icons.check
-                  ),
-                  Icon(Icons.close),
-                  Icon(Icons.question_mark)
-                ]),
-              )));
+      body: Center(
+        child: Text(text),
+      ),
+    );
   }
 }
