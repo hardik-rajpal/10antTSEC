369045a2fa0d89d206cf8a6a53b930fbbbeaa27b
scimaths <mevaibhav045@gmail.com>
2023-02-03 04:49:39 +0530

    Fixed more endpoints

--- backend/models/group_flat_forum.js
+++ backend/models/group_flat_forum.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 
 const GroupFlatForumSchema = new mongoose.Schema(
   {
-    flat: {type: mongoose.Schema.Types.ObjectId, ref: "FlatSchema"},
-    group: {type: mongoose.Schema.Types.ObjectId, ref: "GroupSchema"},
+    flat: {type: String},
+    group: {type: String},
     message: {type: String},
-    sender: {type: mongoose.Schema.Types.ObjectId, ref: "UserSchema"},
+    sender: {type: String},
   },
   { collection: "groupflatforums", timestamps: true }
 );
 
 const model = mongoose.model("GroupFlatForumSchema", GroupFlatForumSchema);
 
-module.exports = {'model': model, 'schema': UserSchema};
+module.exports = model;
